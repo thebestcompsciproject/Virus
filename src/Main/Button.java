68a6be7a0268871ca93d68c629105eaf7f25d949
Ricky Fok <ricky.y.fok@gmail.com>
2018-05-16 15:41:41 -0700

    5/16/18
    basic layout of the button class. Need to implement code for the methods.
    
    Are we adding another version of the button when it is clicked or not??

---
+++ src/Main/Button.java
@@ -0,0 +1,60 @@
+package Main;
+
+import javax.swing.ImageIcon;
+
+public class Button
+{
+
+	private double xCord;
+	private double yCord;
+	private double width;
+	private double height;
+	private String buttonName1;
+	private String buttonName2;
+	
+	private ImageIcon upButton;
+	private ImageIcon downButton;
+	
+
+	public Button(String file1, String file2)
+	{
+		buttonName1 = file1 + ".png";
+		buttonName2 = file2 + ".png";	
+		
+		upButton = new ImageIcon(buttonName1);
+		downButton = new ImageIcon(buttonName2);
+	}
+	
+	
+	/*
+	 * Check if the cursor is in the area of the button
+	 * 
+	 * 	**INCOMPLETE**
+	 */
+	public boolean cursorContains(double x, double y)
+	{
+		return true;
+	}
+	
+	
+	/*
+	 * Constructs the button in the JPanel
+	 * 
+	 * 	**INCOMPLETE**
+	 */
+	public void constructButton()
+	{
+		
+	}
+	
+	/*
+	 * Switches the buttons
+	 * 
+	 * 	**INCOMPLETE**
+	 */
+	public switchButton()
+	{
+		
+	}
+	
+}
