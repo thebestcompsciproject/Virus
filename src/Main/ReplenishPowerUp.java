638c11d0a886c1cde03e4fb8b383282b1ff11a36
Ricky Fok <ricky.y.fok@gmail.com>
2018-05-29 14:37:34 -0700

    5/29/18
    Copy basic stuff for replenishPU

--- src/Main/ReplenishPowerUp.java
+++ src/Main/ReplenishPowerUp.java
@@ -1,6 +1,38 @@
+package Main;
 import java.awt.Color;
+import java.awt.Graphics;
 
-public class ReplenishPowerUp ex
+public class ReplenishPowerUp extends GameObject
 {
-
+	private double radius;
+	private long spawnTime;
+	
+	public ReplenishPowerUp( double x, double y, double direction, double radius, Color color)
+	{
+		super();
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+		this.color = color;
+		this.radius = radius;
+		spawnTime = System.currentTimeMillis();
+	}
+	
+	public void draw(Graphics g)
+	{
+		g.setColor(color);
+		g.fillOval((int) x, (int) y, (int)(2*radius), (int) (2*radius));
+		g.setColor(Color.BLACK);
+		g.drawOval((int) x, (int) y, (int)(2*radius), (int) (2*radius));
+	}
+	
+	public void update()
+	{
+		
+	}
+	
+	public double getRadius()
+	{
+		return radius;
+	}
 }
